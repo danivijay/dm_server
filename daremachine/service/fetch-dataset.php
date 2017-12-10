7292c3abf77634e4369f8ead85e5e44e02429eac
danimvijay <danimvijay@gmail.com>
2017-12-10 17:03:55 +0530

    add sample response

--- daremachine/service/fetch-dataset.php
+++ daremachine/service/fetch-dataset.php
@@ -1,5 +1,25 @@
 <?php
 
-echo '{ "name":"John", "age":30, "car":null };';
+/*
+Value format : 
+1. id
+Eg: "101" 
+Note: Unique ID
+
+2. dare
+Eg:"sing a song"
+
+3. gender
+Eg: "m" 
+Note: m: Male, f: Female, g: General
+Values : m,f,g
+
+4. intensity
+Eg: "1"
+Note: 1:simple, 2: medium, 3: hardcore
+*/
+
+$arr = array("id"=>"101", "dare"=>"sing a song", "gender"=>"g", "intensity"=>"1");
+echo json_encode($arr);
 
 ?>
